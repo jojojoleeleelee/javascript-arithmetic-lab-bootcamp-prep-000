e3e3a8e2da9f664ee5a0ae848c3166518ce3828e
Joanne Lee <vernapii@gmail.com>
2018-03-11 15:12:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ function add (a,b) { // add a and b and return the result.
 number=a+b;
 return number
 }
+var number;
+function subtract (a,b) {//subtract a and b and return the result.
+number=a-b;
+return number
+}
+var number;
+function multiply (a,b) {//multiply a and b and return the result.
+number=a*b;
+return number
+}
+var number;
+function divide (a,b) {//divide a and b and return the result.
+number=a/b;
+return number
+}
+var number;
+function increments (a,b) {//increments a and returns the result.
+number=a+1;
+return number
+}
